7fe44815b9fff837e3098a427e375c0901ca556b
AlexSapoznikov <alex@nodeswat.com>
2019-12-10 11:25:14 +0200

    Add bootstrap

--- next.config.js
+++ next.config.js
@@ -1,28 +1,26 @@
 /**
- * Next-sass config file: https://github.com/zeit/next-plugins/tree/master/packages/next-sass
+ * Next config file
  */
 
 const withSass = require('@zeit/next-sass');
 
-const resourcesLoader = {
-  loader: 'sass-resources-loader',
-  options: {
-    resources: [
-      './styles/_import.scss',
-    ],
-  },
-};
 module.exports = withSass({
   webpack: (config /* , options */) => {
-    config.module.rules.map(rule => {
-      if (
-        rule.test.source.includes('scss') ||
-        rule.test.source.includes('sass')
-      ) {
-        rule.use.push(resourcesLoader);
-      }
-      return rule;
-    });
+    config.module.rules.push(
+      {
+        test: /\.(scss|sass)/,
+        use: [
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: [
+                './styles/_import.scss',
+              ],
+            },
+          },
+        ],
+      },
+    );
     return config;
   },
 });
